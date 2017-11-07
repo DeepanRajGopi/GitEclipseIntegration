2a3cee71f7266185d5963cfc09d77c240370e7ad
DeepanRajGopi <deepanraj395@gmail.com>
2017-11-07 15:20:56 +0530

    Create Sample.js
    Sample JS Initial Commit

---
+++ Sample.js
@@ -0,0 +1,17 @@
+<html>
+<body>
+
+<p id="demo"></p>
+
+<script>
+
+var carName1 = "BMW";
+var carName2 = 'Audi';
+
+document.getElementById("demo").innerHTML =
+carName1 + "<br>" + carName2; 
+
+</script>
+
+</body>
+</html>
